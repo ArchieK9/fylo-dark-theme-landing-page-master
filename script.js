a8d228fe2bd2939e2d20e8f8e0e22bc63960ff61
ArchieK9 <108101526+ArchieK9@users.noreply.github.com>
2023-10-26 15:14:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+let email = document.getElementById("email")
+let dangerText = document.querySelector(".error")
+let submitBtn = document.querySelector(".btn")
+
+submitBtn.addEventListener( "mouseover" ,(e)=>{
+
+    email.validity.typeMismatch ?
+    (e.preventDefault(),
+    dangerText.style.opacity = 1):
+    (dangerText.style.opacity = 0);
+    // if (email.validity.typeMismatch) {
+    //     e.preventDefault();
+    //     dangerText.style.opacity = 1;
+
+    // } else {
+    //     dangerText.style.opacity = 0;
+    // }
+})
